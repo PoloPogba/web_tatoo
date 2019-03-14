9376ca3445f0e60a6ae60224de13d9a3d809e14a
PoloPogba <polo_pogba@yahoo.com>
2019-03-14 13:29:21 +0100

    dropdown

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_145641) do
+ActiveRecord::Schema.define(version: 2019_03_12_163335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,21 @@ ActiveRecord::Schema.define(version: 2019_03_12_145641) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["who_id"], name: "index_activities_on_who_id"
+  end
+
+  create_table "devis", force: :cascade do |t|
+    t.bigint "who_id"
+    t.bigint "activity_id"
+    t.bigint "type_id"
+    t.bigint "function_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_devis_on_activity_id"
+    t.index ["function_id"], name: "index_devis_on_function_id"
+    t.index ["type_id"], name: "index_devis_on_type_id"
+    t.index ["user_id"], name: "index_devis_on_user_id"
+    t.index ["who_id"], name: "index_devis_on_who_id"
   end
 
   create_table "functions", force: :cascade do |t|
@@ -40,7 +55,24 @@ ActiveRecord::Schema.define(version: 2019_03_12_145641) do
     t.index ["activity_id"], name: "index_types_on_activity_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "phone_number"
+    t.string "company_name"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   create_table "whos", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
