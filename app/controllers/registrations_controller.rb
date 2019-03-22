38b322b844f85076ca3f40662190c9a4fca930a3
PoloPogba <polo_pogba@yahoo.com>
2019-03-22 13:11:13 +0100

    controlool

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,8 +7,10 @@ class RegistrationsController < Devise::RegistrationsController
             @id_guest = Guest.last.id
             @id_user = User.last.id
             @form = Form.find_by(guest_id: @id_guest)
+            if @form.user_id == nil
             @form.user_id = @id_user
             @form.save
+            end
             
            
          
@@ -19,7 +21,7 @@ class RegistrationsController < Devise::RegistrationsController
 
     def after_sign_up_path_for(resource)
         @form = Form.find_by(user_id: current_user.id)
-        if @form != nil 
+        if @form == nil 
         root_path
         else 
         formfuction_path(current_user.id)  
